b4654693d13fc936490b9701d87151f0a61d0728
Scott Rigby <scott@r6by.com>
2019-02-10 19:21:02 -0500

    Fix licenses, and standardize on 'Copyright The Helm Authors'
    Signed-off-by: Scott Rigby <scott@r6by.com>

--- pkg/upload/main.go
+++ pkg/upload/main.go
@@ -1,14 +1,29 @@
+// Copyright The Helm Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package upload
 
 import (
 	"context"
 	"fmt"
-	"github.com/helm/chart-releaser/pkg/config"
 	"io/ioutil"
 	"log"
 	"os"
 	"path"
 	"path/filepath"
+
+	"github.com/helm/chart-releaser/pkg/config"
 
 	"github.com/ghodss/yaml"
 	gh "github.com/google/go-github/github"
