4792eb17231c173b6904bad08b5cbfe9ddb24976
Paul Czarkowski <username.taken@gmail.com>
2020-08-06 13:45:58 -0500

    move functionality outside of cli, add tests
    Signed-off-by: Paul Czarkowski <username.taken@gmail.com>

--- cr/cmd/package.go
+++ cr/cmd/package.go
@@ -15,62 +15,35 @@
 package cmd
 
 import (
-	"fmt"
-	"os"
-	"path/filepath"
-
 	"github.com/helm/chart-releaser/pkg/config"
+	"github.com/helm/chart-releaser/pkg/packager"
 	"github.com/spf13/cobra"
-	"github.com/ulule/deepcopier"
-	"helm.sh/helm/v3/pkg/action"
 )
 
 // packageCmd represents the package command
 var packageCmd = &cobra.Command{
-	Use:   "package [/path/to/chart1] [/path/to/chart/2]",
+	Use:   "package [CHART_PATH] [...]",
 	Short: "Package Helm charts",
-	Long: `Package Helm charts ready for release.
-If you wish to use advanced packaging
-options such as creating signed packages or
-updating chart dependencies please use
-"helm package" instead.`,
-	RunE: func(cmd *cobra.Command, args []string) error {
-		var settings map[string]interface{}
-		var err error
+	Long: `This command packages a chart into a versioned chart archive file. If a path
+is given, this will look at that path for a chart (which must contain a
+Chart.yaml file) and then package that directory.
 
+
+If you wish to use advanced packaging options such as creating signed
+packages or updating chart dependencies please use "helm package" instead.`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		var err error
+		if len(args) == 0 {
+			args = append(args, ".")
+		}
 		config, err := config.LoadConfiguration(cfgFile, cmd, getRequiredPackageArgs())
 		if err != nil {
 			return err
 		}
-		client := action.NewPackage()
-		// valueOpts := &values.Options{}
 
-		if config.PackagePath == "" {
-			config.PackagePath = "."
-		}
-		client.Destination = config.PackagePath
-		charts := args
-		if len(charts) == 0 {
-			charts[0] = "."
-		}
-		for i := 0; i < len(charts); i++ {
-			path, err := filepath.Abs(charts[i])
-			if err != nil {
-				return err
-			}
-			if _, err := os.Stat(args[0]); err != nil {
-				return err
-			}
-			deepcopier.Copy(config).To(settings)
-			packageRun, err := client.Run(path, settings)
+		p := packager.NewPackager(config, args)
+		return p.CreatePackages()
 
-			if err != nil {
-				return err
-			}
-
-			fmt.Printf("Successfully packaged chart in %s and saved it to: %s\n", path, packageRun)
-		}
-		return nil
 	},
 }
 
---
+++ pkg/packager/packager.go
@@ -0,0 +1,68 @@
+// Copyright The Helm Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package packager
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/helm/chart-releaser/pkg/config"
+	"github.com/ulule/deepcopier"
+	"helm.sh/helm/v3/pkg/action"
+)
+
+// Packager exposes the packager object
+type Packager struct {
+	config *config.Options
+	paths  []string
+}
+
+// NewPackager does a thing
+func NewPackager(config *config.Options, paths []string) *Packager {
+	return &Packager{
+		config: config,
+		paths:  paths,
+	}
+}
+
+// CreatePackages creates Helm chart packages
+func (p *Packager) CreatePackages() error {
+	var settings map[string]interface{}
+
+	helmClient := action.NewPackage()
+
+	helmClient.Destination = p.config.PackagePath
+
+	for i := 0; i < len(p.paths); i++ {
+		path, err := filepath.Abs(p.paths[i])
+		if err != nil {
+			return err
+		}
+		if _, err := os.Stat(p.paths[i]); err != nil {
+			return err
+		}
+		deepcopier.Copy(p.config).To(settings)
+		packageRun, err := helmClient.Run(path, settings)
+
+		if err != nil {
+			fmt.Printf("Failed to package chart in %s (%s)\n", path, err.Error())
+			return err
+		}
+
+		fmt.Printf("Successfully packaged chart in %s and saved it to: %s\n", path, packageRun)
+	}
+	return nil
+}
