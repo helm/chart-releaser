48286387685d100e624f33f1fb60a4dfedc4c006
Reinhard Nägele <unguiculus@gmail.com>
2020-10-29 18:07:10 +0100

    Comment out linting
    Signed-off-by: Reinhard Nägele <unguiculus@gmail.com>

--- magefile.go
+++ magefile.go
@@ -35,12 +35,12 @@ func Lint() error {
 	if err := sh.RunV("golangci-lint", "run", "--timeout", "3m"); err != nil {
 		return err
 	}
-	//if err := sh.RunV("go", "vet", "-v", "./..."); err != nil {
-	//	return err
-	//}
-	//if err := sh.RunV("goimports", "-w", "-l", "."); err != nil {
-	//	return err
-	//}
+	if err := sh.RunV("go", "vet", "-v", "./..."); err != nil {
+		return err
+	}
+	if err := sh.RunV("goimports", "-w", "-l", "."); err != nil {
+		return err
+	}
 	if err := sh.RunV("go", "mod", "tidy"); err != nil {
 		return err
 	}
