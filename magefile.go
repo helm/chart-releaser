d35bb73395201a596694fcc387a54a52ebaeb504
Reinhard Nägele <unguiculus@gmail.com>
2020-10-29 17:55:00 +0100

    Re-add 'go mod tidy', remove 'go imports'
    Signed-off-by: Reinhard Nägele <unguiculus@gmail.com>

--- magefile.go
+++ magefile.go
@@ -38,7 +38,10 @@ func Lint() error {
 	if err := sh.RunV("go", "vet", "-v", "./..."); err != nil {
 		return err
 	}
-	if err := sh.RunV("goimports", "-w", "-l", "."); err != nil {
+	//if err := sh.RunV("goimports", "-w", "-l", "."); err != nil {
+	//	return err
+	//}
+	if err := sh.RunV("go", "mod", "tidy"); err != nil {
 		return err
 	}
 	return sh.RunV("git", "diff", "--exit-code")
