013e4273e972b901bd91d5cef7f2934da3eeef01
Reinhard Nägele <unguiculus@gmail.com>
2021-02-17 19:38:36 +0100

    Add missing tool dependencies to tools.go (#104)
    This fixes the issue that releases with GoReleaser would fail because
    the repo wasn't in a clean state due to `go.mod`/`go.sum`being modified.
    
    Signed-off-by: Reinhard Nägele <unguiculus@gmail.com>

--- pkg/tools/tools.go
+++ pkg/tools/tools.go
@@ -17,6 +17,8 @@
 package tools
 
 import (
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/goreleaser/goreleaser"
+	_ "github.com/magefile/mage"
 	_ "golang.org/x/tools/cmd/goimports"
 )
