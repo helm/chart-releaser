6632d4de3eb51c8e732712a5688bc5c5f7eb82b1
Kenneth Bingham <kenneth.bingham@netfoundry.io>
2024-09-26 10:26:53 -0400

    tweak build to enable contributions from forks (#440)
    Signed-off-by: Kenneth Bingham <kenneth.bingham@netfoundry.io>

--- magefile.go
+++ magefile.go
@@ -137,5 +137,12 @@ func Release() error {
 		return err
 	}
 
-	return sh.RunV("goreleaser", "release", "--clean")
+	var args []string
+	args = append(args, "release", "--clean")
+
+	if os.Getenv("GITHUB_REPOSITORY") != "helm/chart-releaser" {
+		args = append(args, "--skip=docker,homebrew")
+	}
+
+	return sh.RunV("goreleaser", args...)
 }
